afb1e123ce0871eada78a80819257927c4827571
Devu Anil <devuanil369@gmail.com>
2025-03-31 14:17:10 +0530

    Added build.sh for Render deployment

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-uy_fdia2e#jc^0&69hvns7-umyrd6^=)lc_n=t49s$^8rg1ztz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleWare",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
